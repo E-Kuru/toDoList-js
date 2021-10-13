16aa4952ca03dfc86449ca449d1cf200a86ffb2d
E-Kuru <evandalleaublop@gmail.com>
2021-10-13 17:30:27 +0200

    Filter Done

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var thisTask = document.getElementById('newTask')
 var container = document.getElementById('my-tasks')
 var oneTask = document.getElementsByClassName('task')
+var allStatus = document.getElementById('status-select')
 var tasks = []
 
 function onTaskSubmit(){
     var input = thisTask.value
-    var taskAdder = {value : input, status : 'To Do'}
+    var taskAdder = {value : input, status : 'To do'}
     thisTask.value = ''
     
     tasks.push(taskAdder)
@@ -14,6 +15,8 @@ function onTaskSubmit(){
     container.innerHTML = "";
     adderHtml(tasks,container);
 }
+
+// Ajoute une élément html à partir d'un tableau en récupérant l'index
 
 function adderHtml(array,theDiv){
     array.forEach(function (e){
@@ -31,6 +34,8 @@ function deleteTask (){
     console.log('It clicked');
 }
 
+// Ajoute mon élément qui modifiera ma task et son status 
+
 function modifyHtml (index){
 
     // container.innerHTML = "";
@@ -43,6 +48,8 @@ function modifyHtml (index){
         </form>
     </div>`
 }
+
+// Valide la modification du status et de la value 
 
 function modifyTask (index){    
 
@@ -64,4 +71,37 @@ function modifyTask (index){
     
     container.innerHTML = "";
     adderHtml(tasks,container)
+}
+
+function getStatus (theStatus){
+    var todo = tasks.filter(function(stat){
+        return stat.status === theStatus
+    })
+    var doing = tasks.filter(function(stat){
+        return stat.status === theStatus
+    })
+    var done = tasks.filter(function(stat){
+        return stat.status === theStatus
+    })
+
+    if(theStatus === 'To do'){
+        console.log(theStatus);
+        container.innerHTML = ''
+        adderHtml(todo,container)
+    }
+    else if (theStatus === 'doing'){
+        console.log(theStatus);
+        container.innerHTML = ''
+        adderHtml(doing,container)
+    }
+    else if (theStatus === 'done'){
+        console.log(theStatus);
+        container.innerHTML = ''
+        adderHtml(done,container)
+    }
+    else{
+        console.log('all');
+        container.innerHTML = ''
+        adderHtml(tasks,container)
+    }
 }
